b42255e36c04c66f1976ff30dcf3255378d32f64
Vishal Pandey <vishalpandey2119@gmail.com>
2025-05-19 11:30:02 +0530

    some final changes updated

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -2,7 +2,7 @@
 
 const Sidebar = ({SelectedPost,setSelectedPost}) =>{
 
-return <div className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar" style={{width: "280px"}}>
+return <div className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar" >
     <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
       <svg className="bi pe-none me-2" width="40" height="32" aria-hidden="true"><use xlinkHref="#bootstrap"></use></svg>
       <span className="fs-4">VishalMedia</span>
